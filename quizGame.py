93b12950d4d6e66d20e6a5105a1a9ef3e3bc7169
Justin Jones <jus.jones@yahoo.com>
2024-07-24 21:45:01 -0500

    finishing up writing program;

---
+++ quizGame.py
@@ -0,0 +1,51 @@
+print("welcome to my computer quiz!")
+score = 0 
+
+playing = input("Would you like to play this game? ")
+
+if playing.upper() != "yes".upper():
+    quit()
+
+print("okay lets play!")
+
+answer = input("What does CPU stand for? ")
+
+if answer.upper() == "central processing unit".upper(): 
+    score += 1
+    print("Correct Answer! ")
+else:
+    print("Incorrect answer!")
+    
+print(score)
+
+answer = input("What does RAM stand for? ")
+
+if answer.upper() == "Random Access Memory".upper(): 
+    score += 1
+    print("Correct Answer! ")
+else:
+    print("Incorrect answer!")
+    
+print(score)
+
+answer = input("What does PSU stand for? ")
+
+if answer.upper() == "Power Supply Unit".upper(): 
+    score += 1
+    print("Correct Answer! ")
+else:
+    print("Incorrect answer!")
+    
+print(score)
+
+answer = input("What does GPU stand for? ")
+
+if answer.upper() == "graphics processing unit".upper(): 
+    score += 1
+    print("Correct Answer! ")
+else:
+    print("Incorrect answer!")
+    
+print(f"Your final score is: {score}")
+
+
